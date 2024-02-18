8c29fea81ed73ecaaa3b6240662452bfe0348277
vasiioandre <vasi_ioandre@yahoo.com>
2024-02-18 20:22:20 +0200

    Logging

--- src/main/java/com/ioana/springboot/todoapp/login/LoginController.java
+++ src/main/java/com/ioana/springboot/todoapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.ioana.springboot.todoapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +9,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-	
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
 	@RequestMapping("login")
 	public String goToLoginPage(@RequestParam String name, ModelMap model) {
 		model.put("name", name);
+		
+		logger.debug("Request param is {}", name);
+		logger.info("I want this printed at info level");
+		logger.warn("I want this printed at warn level");
 		
 		return "login";
 	}
